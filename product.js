691799cdee43616e286cfb981fb53f8edd2d3e8a
ridwantajudeen <ridwantajudeen8@gmail>
2022-10-01 06:56:43 -0700

    u

--- product.js
+++ product.js
@@ -1,13 +1,33 @@
 let num = document.getElementById("num")
 let plus = document.getElementById("plus")
 let minus = document.getElementById("minus")
+let descPrice = document.getElementById("desc-price")
+let order = document.getElementById("order-btn")
 let number = 0
 num.textContent= number
+let priceonee = 1000
+let priceone =1000
+
 minus.addEventListener("click", function(){
+  
   number -= 1
+  if (number <= 1) {
+    number = 0;
+    priceone = 1000
+    
+ 
+}
+  priceone = priceone - priceonee 
    num.textContent = number
+   descPrice.textContent= ("N"+priceone)
 })
 plus.addEventListener("click", function(){
   number += 1
+  priceone = priceone + priceonee
    num.textContent = number
+   descPrice.textContent= ("N"+priceone)
+})
+
+order.addEventListener("click", function(){
+  alert("Product not Available")
 })
